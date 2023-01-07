6d51409e6d6989920867683d11df8d578920b850
ManOnTopain <116823830+ManOnTopain@users.noreply.github.com>
2023-01-07 14:22:52 -0400

    Update AnyGame.lua

--- Escripts/AnyGame.lua
+++ Escripts/AnyGame.lua
@@ -1,3 +1,26 @@
+local whitelistednigerians = loadstring(game:HttpGet('https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/Table.lua'))()
+local Blacklistednigerians = loadstring(game:HttpGet('https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/OtherTable.lua'))()
+local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
+local players = game:GetService("Players")
+local lplr = players.LocalPlayer
+local GuiL = shared.CoGuiLibrary
+
 if game.ReplicatedStorage:FindFirstChild("TS") then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/Escripts/bedwars.lua')()
+    if hwid == whitelistednigerians then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/Escripts/bedwars.lua')()
+    end
+end
+
+  
+  --[[
+  examples:
+  GuiL.Button("TabName", "Name", function()
+    --script here
+  end)
+]]--
+  
+if hwid == Blacklistednigerians then
+    lplr:Kick("Ur an blacklisted nigerian get outa here")
+else
+    --script here
 end
