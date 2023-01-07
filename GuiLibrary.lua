afb7ced30367a3ab01b95e3a815b43000a321747
ManOnTopain <116823830+ManOnTopain@users.noreply.github.com>
2023-01-07 13:59:08 -0400

    Update GuiLibrary.lua

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -1,1 +1,18 @@
-
+shared.CoGuiLibrary = {
+	['Button'] = function(Tab, Name, Callback)
+		module.Parent = game.CoreGui.Cola:FindFirstChild(Tab)
+		module.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		module.Position = UDim2.new(0.00518134702, 0, 0.0778540969, 0)
+		module.Size = UDim2.new(0, 193, 0, 41)
+		module.Font = Enum.Font.SourceSans
+		module.Text = Name
+		module.TextColor3 = Color3.fromRGB(255, 255, 255)
+		module.TextScaled = true
+		module.TextSize = 14.000
+		module.TextWrapped = true
+		module.Name = Name
+		module.MouseButton1Click:Connect(function()
+		pcall(Callback)
+		end)
+	end
+}
