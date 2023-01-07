7401dc875593c070d009d14dffcec06400d78c89
RealMaster <119766152+ActualMaster@users.noreply.github.com>
2023-01-07 13:34:30 -0600

    Update AnyGame.lua

--- Escripts/AnyGame.lua
+++ Escripts/AnyGame.lua
@@ -8,7 +8,7 @@ local workspace = game.Workspace
 
 if game.ReplicatedStorage:FindFirstChild("TS") then
     if hwid == whitelistednigerians then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/Escripts/bedwars.lua')()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/Escripts/bedwars.lua')()
     end
 end
 
