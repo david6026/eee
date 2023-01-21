26edff8bdd2bd89f448c57b3ee6de5e4c09feb18
Broski <113543570+Nebulaprivatewhitelistbypass@users.noreply.github.com>
2023-01-21 20:30:29 +0100

    Create mainloader.lua

---
+++ mainloader.lua
@@ -0,0 +1,14 @@
+if game.PlaceId == 6872265039 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/lobbytab.lua",true))()
+
+end
+
+if game.PlaceId == 6872274481 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/skywarstab.lua",true))()
+
+end
+
+if game.PlaceId == 6872274481 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebulaprivatewhitelistbypass/eee/main/bwtab.lua",true))()
+
+end
